6a628b82fdf931d720c58038dd0a4b8d4c9b75d9
Andrei Shumailov <gentoo1993@gmail.com>
2020-04-07 13:42:32 +0200

    Fix gnome keybindings

--- home-manager/configs/dconf.nix
+++ home-manager/configs/dconf.nix
@@ -1,4 +1,5 @@
-{ ... }: {
+{ lib, ... }: {
+
 
   dconf.settings = {
     "org/gnome/settings-daemon/plugins/xsettings" = {
@@ -29,42 +30,47 @@
     "org/gnome/desktop/input-sources" = {
       per-window=false;
       show-all-sources=false;
-      sources="[(\\'xkb\\', \\'us\\'), (\\'xkb\\', \\'de\\'), (\\'xkb\\', \\'ru\\')]";
-      xkb-options="[\\'grp:alt_space_toggle\\']";
+      # todo this will work afte 20.03 release
+      #sources=[ 
+      #  lib.hm.gvariant.mkTuple ["xkb" "us"] 
+      #  lib.hm.gvariant.mkTuple ["xkb" "de"] 
+      #  lib.hm.gvariant.mkTuple ["xkb" "ru"] 
+      #];
+      xkb-options=["grp:alt_space_toggle"];
     };
     "org/gnome/shell/keybindings" = {
-      toggle-application-view="[\\'<Super>d\\']";
+      toggle-application-view=["<Super>d"];
     };
     "org/gnome/settings-daemon/plugins/media-keys" = {
-      screensaver="[\\'<Primary><Super>l\\']";
+      screensaver=["<Primary><Super>l"];
     };
     "org/gnome/mutter/keybindings" = {
-      toggle-tiled-left="[\\'<Primary><Super>Left\\']";
-      toggle-tiled-right="[\\'<Primary><Super>Right\\']";
+      toggle-tiled-left=["<Primary><Super>Left"];
+      toggle-tiled-right=["<Primary><Super>Right"];
     };
     "org/gnome/desktop/wm/keybindings" = {
-      close="[\\'<Shift><Super>c\\']";
-      maximize="[\\'<Primary><Super>Up\\']";
-      move-to-monitor-down="@as []";
-      move-to-monitor-up="@as []";
-      move-to-workspace-down="[\\'<Shift><Super>Down\\']";
-      move-to-workspace-up="[\\'<Shift><Super>Up\\']";
-      switch-to-workspace-down="[\\'<Super>Down\\']";
-      switch-to-workspace-up="[\\'<Super>Up\\']";
-      toggle-fullscreen="[\\'<Super>f\\']";
-      unmaximize="[\\'<Primary><Super>Down\\']";
-      activate-window-menu="@as []";
-      begin-resize="[\\'<Super>r\\']";
-      move-to-monitor-left="@as []";
-      move-to-monitor-right="@as []";
-      switch-input-source="[\\'<Alt>space\\']";
-      switch-input-source-backward="[\\'<Shift><Alt>space\\']";
-      switch-to-workspace-left="@as []";
-      switch-to-workspace-right="@as []";
-      toggle-maximized="@as []";
+      close=["<Shift><Super>c"];
+      maximize=["<Primary><Super>Up"];
+      move-to-monitor-down=["@as []"];
+      move-to-monitor-up=["@as []"];
+      move-to-workspace-down=["<Shift><Super>Down"];
+      move-to-workspace-up=["<Shift><Super>Up"];
+      switch-to-workspace-down= ["<Super>Down"];
+      switch-to-workspace-up=["<Super>Up"];
+      toggle-fullscreen=["<Super>f"];
+      unmaximize=["<Primary><Super>Down"];
+      activate-window-menu=["@as []"];
+      begin-resize=["<Super>r"];
+      move-to-monitor-left=["@as []"];
+      move-to-monitor-right=["@as []"];
+      switch-input-source=["<Alt>space"];
+      switch-input-source-backward=["<Shift><Alt>space"];
+      switch-to-workspace-left=["@as []"];
+      switch-to-workspace-right=["@as []"];
+      toggle-maximized=["@as []"];
     };
     "org/gnome/desktop/search-providers" = {
-      sort-order="[\\'org.gnome.Contacts.desktop\\', \\'org.gnome.Documents.desktop\\', \\'org.gnome.Nautilus.desktop\\']";
+      sort-order=["org.gnome.Contacts.desktop" "org.gnome.Documents.desktop"  "org.gnome.Nautilus.desktop"];
     };
 
     "org/gnome/desktop/peripherals/touchpad" = {
