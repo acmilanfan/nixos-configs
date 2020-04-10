563fcdfd754630ef295d88deb2e49446224c221d
Andrei Shumailov <gentoo1993@gmail.com>
2020-04-10 16:47:04 +0200

    Exclude unused gnome packages

--- nixos/configs/gnome.nix
+++ nixos/configs/gnome.nix
@@ -1,4 +1,4 @@
-{ ... }: {
+{ pkgs, ... }: {
 
   services.xserver.desktopManager.gnome3.enable = true;
 
@@ -6,8 +6,18 @@
     #todo
   };
 
-  environment.gnome3.excludePackages = [
-    #todo
+  environment.gnome3.excludePackages = with pkgs.gnome3; [
+    totem
+    gnome-software
+    cheese
+    geary
+    gnome-photos
+    gnome-contacts
+    gnome-calendar
+    gnome-maps
+    gnome-music
+    seahorse
+    simple-scan
   ];
 
 }
