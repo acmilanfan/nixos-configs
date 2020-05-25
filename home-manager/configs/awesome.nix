75ce8b04c6bbd81c7a6a632efc54a71a9c2bd015
Andrei Shumailov <gentoo1993@gmail.com>
2020-05-25 12:55:18 +0200

    Add sourcing for power_widget.lua

--- home-manager/configs/awesome.nix
+++ home-manager/configs/awesome.nix
@@ -1,7 +1,8 @@
-{ ... }: {
+{ pkgs, ... }: {
 
   xdg.configFile = {
     "awesome/rc.lua".source = ../../dotfiles/awesome/rc.lua;
+    "awesome/power_widget.lua".source = ../../dotfiles/awesome/power_widget.lua;
     "awesome/themes/purple/assets.lua".source = ../../dotfiles/awesome/themes/purple/assets.lua;
     "awesome/themes/purple/theme.lua".source = ../../dotfiles/awesome/themes/purple/theme.lua;
   };
