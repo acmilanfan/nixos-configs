5548c949747110af84a31ed85f15af5ab52120cf
Andrei Shumailov <gentoo1993@gmail.com>
2020-05-25 12:51:24 +0200

    Fix xinput mouse name

--- nixos/configs/xserver-libinput.nix
+++ nixos/configs/xserver-libinput.nix
@@ -9,5 +9,6 @@
 
   services.xserver.displayManager.sessionCommands = ''
     xinput --set-prop '2.4G Mouse' 'libinput Natural Scrolling Enabled' 0
+    xinput --set-prop '2.4G Mouse Mouse' 'libinput Natural Scrolling Enabled' 0
   ''; 
 }
