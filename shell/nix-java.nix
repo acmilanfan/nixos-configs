db444d80d16750999eeaf30f1cc9da31df5a12f9
Andrei Shumailov <andrei.shumailov@auto1.com>
2023-01-07 18:35:17 +0100

    Update Java shell

--- shell/nix-java.nix
+++ shell/nix-java.nix
@@ -9,7 +9,7 @@ let fhs = pkgs.buildFHSUserEnv {
   name = "java-maven-env";
   targetPkgs = pkgs: (with pkgs;
     [
-      maven zlib pam gdb xorg.libXext xorg.libX11 xorg.libXrender xorg.libXtst xorg.libXi freetype gradle 
+      maven zlib pam gdb xorg.libXext xorg.libX11 xorg.libXrender xorg.libXtst xorg.libXi freetype gradle libaio numactl ncurses5
     ]);
   runScript = "bash";
 };
