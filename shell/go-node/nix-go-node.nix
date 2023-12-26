b024e02ddb4712919af87adc879e7e5d4bc7085f
Andrei Shumailov <gentoo1993@gmail.com>
2023-12-26 15:23:38 +0100

    add go and node shell

---
+++ shell/go-node/nix-go-node.nix
@@ -0,0 +1,20 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let fhs = pkgs.buildFHSUserEnv {
+  name = "dev-env";
+  targetPkgs = pkgs: (with pkgs;
+    [
+      go nodejs yarn ungoogled-chromium
+    ]);
+  runScript = "bash";
+};
+in pkgs.stdenv.mkDerivation {
+  name = "maven-shell";
+  nativeBuildInputs = [ fhs ];
+  shellHook = ''
+    export LANG=en_US.UTF-8
+    export TZ=Europe/Berlin
+    # npm set prefix ~/.npm-global
+    exec dev-env
+  '';
+}
