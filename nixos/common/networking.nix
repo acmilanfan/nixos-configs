b17118b1e5fbcbaf5d5090439e8b600d494d68b3
Andrei Shumailov <gentoo1993@gmail.com>
2024-01-07 12:26:18 +0100

    Update VPN connection

--- nixos/common/networking.nix
+++ nixos/common/networking.nix
@@ -1,7 +1,10 @@
-{ ... }: {
+{ pkgs, ... }: {
 
   networking.networkmanager.enable = true;
   networking.useDHCP = false;
 
   networking.networkmanager.wifi.powersave = false;
+
+  services.globalprotect.enable = true;
+  environment.systemPackages = with pkgs; [ globalprotect-openconnect gp-saml-gui ];
 }
