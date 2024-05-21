35058051c7be3a5e320c193447e7352f29a08818
Andrei Shumailov <gentoo1993@gmail.com>
2024-05-21 08:24:36 +0200

    change grp toggle

--- nixos/common/xserver-xkb.nix
+++ nixos/common/xserver-xkb.nix
@@ -2,7 +2,8 @@
 
   services.xserver.xkb = {
     layout = "us,de,ru";
-    options = "grp:alt_space_toggle,caps:escape";
+    # options = "grp:alt_space_toggle,caps:escape";
+    options = "grp:rctrl_rshift_toggle";
   };
 
 }
