d987902291c9c177f77385891493da5db41260bd
Andrei Shumailov <gentoo1993@gmail.com>
2024-05-21 08:21:28 +0200

    fzf colorscheme

--- nixos/home-manager/common/shell.nix
+++ nixos/home-manager/common/shell.nix
@@ -41,7 +41,7 @@
       compinit
       _comp_options+=(globdots)
 
-      export FZF_DEFAULT_OPTS='--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4'
+      export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS' --color=fg:#d0d0d0,bg:#121212,hl:#5f87af --color=fg+:#d0d0d0,bg+:#262626,hl+:#5fd7ff --color=info:#afaf87,prompt:#d7005f,pointer:#af5fff --color=marker:#87ff00,spinner:#af5fff,header:#87afaf'
       export TERM='xterm-256color'
 
       # Use vim keys in tab complete menu
