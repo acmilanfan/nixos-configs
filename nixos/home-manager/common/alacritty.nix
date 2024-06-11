fb00176014ad99e844d6d1178258fe1999cd9efa
Andrei Shumailov <gentoo1993@gmail.com>
2024-06-11 10:03:25 +0200

    fix alacritty config

--- nixos/home-manager/common/alacritty.nix
+++ nixos/home-manager/common/alacritty.nix
@@ -3,41 +3,36 @@
   programs.alacritty = {
     enable = true;
     settings = {
-      key_bindings = [{
-        key = "I";
-        mods = "Control";
-        chars = "\\x1b[105;5u";
-      }];
       font = { size = 12; };
       selection = { save_to_clipboard = true; };
       colors = {
         primary = {
-          background = "0x1e2127";
-          foreground = "0xabb2bf";
+          background = "#1e2127";
+          foreground = "#abb2bf";
         };
         selection = {
-          background = "0x3e4452";
+          background = "#3e4452";
           text = "CellForeground";
         };
         normal = {
-          black = "0x2c323c";
-          red = "0xe06c75";
-          green = "0x98c379";
-          yellow = "0xe5c07b";
-          blue = "0x61afef";
-          magneta = "0xc678dd";
-          cyan = "0x56b6c2";
-          white = "0xe6efff";
+          black = "#2c323c";
+          red = "#e06c75";
+          green = "#98c379";
+          yellow = "#e5c07b";
+          blue = "#61afef";
+          magenta = "#c678dd";
+          cyan = "#56b6c2";
+          white = "#e6efff";
         };
         bright = {
-          black = "0x3e4452";
-          red = "0xe06c75";
-          green = "0x98c379";
-          yellow = "0xe5c07b";
-          blue = "0x61afef";
-          magneta = "0xc678dd";
-          cyan = "0x56b6c2";
-          white = "0x828791";
+          black = "#3e4452";
+          red = "#e06c75";
+          green = "#98c379";
+          yellow = "#e5c07b";
+          blue = "#61afef";
+          magenta = "#c678dd";
+          cyan = "#56b6c2";
+          white = "#828791";
         };
       };
     };
