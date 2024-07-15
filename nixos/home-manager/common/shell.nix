9d48aa569a51cbe48bfe8be48557c8fc63709ec2
Andrei Shumailov <gentoo1993@gmail.com>
2024-07-15 10:34:32 +0200

    fzf theme

--- nixos/home-manager/common/shell.nix
+++ nixos/home-manager/common/shell.nix
@@ -39,7 +39,29 @@
       compinit
       _comp_options+=(globdots)
 
-      export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS' --color=fg:#d0d0d0,bg:#121212,hl:#5f87af --color=fg+:#d0d0d0,bg+:#262626,hl+:#5fd7ff --color=info:#afaf87,prompt:#d7005f,pointer:#af5fff --color=marker:#87ff00,spinner:#af5fff,header:#87afaf'
+      export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
+        --highlight-line \
+        --info=inline-right \
+        --ansi \
+        --layout=reverse \
+        --border=none
+        --color=bg+:#283457 \
+        --color=bg:#16161e \
+        --color=border:#27a1b9 \
+        --color=fg:#c0caf5 \
+        --color=gutter:#16161e \
+        --color=header:#ff9e64 \
+        --color=hl+:#2ac3de \
+        --color=hl:#2ac3de \
+        --color=info:#545c7e \
+        --color=marker:#ff007c \
+        --color=pointer:#ff007c \
+        --color=prompt:#2ac3de \
+        --color=query:#c0caf5:regular \
+        --color=scrollbar:#27a1b9 \
+        --color=separator:#ff9e64 \
+        --color=spinner:#ff007c \
+      "
       export TERM='xterm-256color'
 
       # Use vim keys in tab complete menu
