174c60de5e00d8e2f89c389dcd903986e7b419d2
Andrei Shumailov <gentoo1993@gmail.com>
2024-10-25 09:33:36 +0200

    docker support in Java

--- nixos/z16/configs/virtualisation.nix
+++ nixos/z16/configs/virtualisation.nix
@@ -1,6 +1,13 @@
 { ... }: {
 
-  # virtualisation.docker.enable = true;
+  virtualisation.docker = {
+    enable = true;
+    rootless = {
+      enable = true;
+      setSocketVariable = true;
+    };
+  };
+
   virtualisation.libvirtd.enable = true;
 
   users.users.gentooway.extraGroups = [ "docker" "libvirtd" ];
--- shell/java/nix-java.nix
+++ shell/java/nix-java.nix
@@ -9,7 +9,7 @@ let fhs = pkgs.buildFHSUserEnv {
     targetPkgs = pkgs: (with pkgs;
       [
         maven zlib pam gdb xorg.libXext xorg.libX11 xorg.libXrender xorg.libXtst xorg.libXi freetype gradle libaio numactl ncurses5 libxcrypt
-        nodejs_18 nodePackages.mocha nodePackages.ts-node nodePackages.typescript
+        nodejs_18 nodePackages.mocha nodePackages.ts-node nodePackages.typescript docker
       ]);
     runScript = "zsh";
   };
@@ -19,6 +19,12 @@ in pkgs.stdenv.mkDerivation {
   shellHook = ''
     export LANG=en_US.UTF-8
     export TZ=Europe/Berlin
+    export PATH=$PATH:/run/current-system/bin
+    export DOCKER_CONFIG=/etc/docker/config.json
+    sudo systemctl start docker
+    sudo systemctl is-active --quiet docker && echo "Docker is running" || echo "Failed to start Docker"
+    sudo chmod 666 /var/run/docker.sock
+    export DOCKER_HOST=unix:///var/run/docker.sock
     exec java-maven-env
   '';
 }
