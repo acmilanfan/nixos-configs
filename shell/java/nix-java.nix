d1faa0d84bb5a2170c5633a1d4495a6bb50a7dfe
Andrei Shumailov <gentoo1993@gmail.com>
2025-03-30 12:59:45 +0200

    update java shell

--- shell/java/nix-java.nix
+++ shell/java/nix-java.nix
@@ -1,19 +1,46 @@
-{ pkgs ? import <nixpkgs>
-  { overlays = [ (self: super: {
-      jdk = super.jetbrains.jdk;
-    }) ];
-  }
-}:
-let fhs = pkgs.buildFHSUserEnv {
+{ pkgs ? import <nixpkgs> {
+  overlays = [ (self: super: { jdk = super.jetbrains.jdk; }) ];
+} }:
+
+let
+  # selectedJDK = pkgs.openjdk11;
+  selectedJDK = pkgs.openjdk21;
+  # selectedJDK = pkgs.openjdk23;
+
+  fhs = pkgs.buildFHSUserEnv {
     name = "java-maven-env";
-    targetPkgs = pkgs: (with pkgs;
-      [
-        maven zlib pam gdb xorg.libXext xorg.libX11 xorg.libXrender xorg.libXtst xorg.libXi freetype gradle libaio
-        numactl ncurses5 libxcrypt libxcrypt-legacy nodejs_18 nodePackages.mocha nodePackages.ts-node
-        nodePackages.typescript docker openssl glibc mysql-client glibcLocales gnumake
+    targetPkgs = pkgs:
+      (with pkgs; [
+        maven
+        zlib
+        pam
+        gdb
+        xorg.libXext
+        xorg.libX11
+        xorg.libXrender
+        xorg.libXtst
+        xorg.libXi
+        freetype
+        gradle
+        libaio
+        numactl
+        ncurses5
+        libxcrypt
+        libxcrypt-legacy
+        nodejs_22
+        nodePackages.mocha
+        nodePackages.ts-node
+        nodePackages.typescript
+        docker
+        openssl
+        glibc
+        mysql-client
+        glibcLocales
+        gnumake
       ]);
     runScript = "zsh";
   };
+
 in pkgs.stdenv.mkDerivation {
   name = "maven-shell";
   nativeBuildInputs = [ fhs ];
@@ -26,7 +53,10 @@ in pkgs.stdenv.mkDerivation {
     sudo systemctl is-active --quiet docker && echo "Docker is running" || echo "Failed to start Docker"
     sudo chmod 666 /var/run/docker.sock
     export DOCKER_HOST=unix:///var/run/docker.sock
+
+    export JAVA_HOME=${selectedJDK}
+    export PATH=$JAVA_HOME/bin:$PATH
+
     exec java-maven-env
   '';
 }
-
