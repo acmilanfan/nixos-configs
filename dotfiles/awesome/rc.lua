41f271b3a4965c7a6dbe173151b01ebbc696a32e
Andrei Shumailov <gentoo1993@gmail.com>
2025-06-05 09:05:57 +0200

    update awesome wm theme

--- dotfiles/awesome/rc.lua
+++ dotfiles/awesome/rc.lua
@@ -1296,3 +1296,13 @@ end)
 -- }}}
 
 update_locker_widget()
+
+client.connect_signal("manage", function(c)
+    c.shape = function(cr, w, h)
+        gears.shape.rounded_rect(cr, w, h, 10)
+    end
+end)
+
+beautiful.notification_shape = function(cr, w, h)
+    gears.shape.rounded_rect(cr, w, h, 10)
+end
--- nixos/common/awesome.nix
+++ nixos/common/awesome.nix
@@ -7,6 +7,14 @@
   };
 
   services.picom.enable = true;
+  services.picom.settings = {
+    use-damage = true;
+    vsync = true;
+    # shadow = true;
+    # backend = "glx";
+    detect-rounded-corners = true;
+    respect-client-shape = true;
+  };
   security.pam.services.i3lock.enable = true;
 
 }
