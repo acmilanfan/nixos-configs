6a6cbb45be810da8f2b0b2069ab71c96f56c5f36
Andrei Shumailov <gentoo1993@gmail.com>
2025-06-27 20:01:03 +0200

    updates

--- nixos/yogabook/home-manager/custom.nix
+++ nixos/yogabook/home-manager/custom.nix
@@ -2,6 +2,13 @@
 
   programs.rofi = { extraConfig = { dpi = 0; }; };
 
-  home.pointerCursor = { size = lib.mkForce 48; };
+  home.pointerCursor = { size = lib.mkForce 32; };
+
+  # home.packages = with pkgs; [ (callPackage ./vantage.nix { }) ];
+
+  # home.file.".config/REAPER/reaper.ini".text = ''
+  #   [.swell]
+  #   ui_scale=1.7 // scales the sizes in libSwell.colortheme
+  # '';
 
 }
