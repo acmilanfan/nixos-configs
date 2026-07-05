9d120a728d5bf3d3c1ca8ec01dd511afde637e97
Andrei Shumailov <gentoo1993@gmail.com>
2026-07-05 14:40:05 +0200

    yb: fix wifi

--- nixos/common/networking.nix
+++ nixos/common/networking.nix
@@ -6,6 +6,8 @@
   networking.networkmanager.wifi.backend = "iwd";
   networking.networkmanager.wifi.powersave = false;
 
+  networking.wireless.iwd.enable = true;
+
   # services.globalprotect.enable = true;
   # environment.systemPackages = with pkgs; [ globalprotect-openconnect gp-saml-gui ];
 }
